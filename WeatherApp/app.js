62238b904fb5ed95a994b6ec04d957ded5a8de54
muzamilsikander <102538533+Muzamil-Sikander@users.noreply.github.com>
2022-10-17 21:37:13 +0500

    Add files via upload

---
+++ WeatherApp/app.js
@@ -0,0 +1,15 @@
+let apikey = "adb4676581b7a9b11a6c8ae382b6dcf3";
+
+function getweather() {
+    let cityname = document.querySelector("#cityname").value;
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}&units=metric`)
+        .then(function (response) {
+            document.querySelector("#data").innerHTML = `${response.data.name} Temp is ${response.data.main.temp}°C`;
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            // document.querySelector("#Wdata").innerHTML = error
+            console.log(error);
+        })
+}
+getweather()
